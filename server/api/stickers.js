c83c1399ee8aa62816872fa4298600923ac90de6
Anthony Lopez <grungekarma@gmail.com>
2021-07-04 01:48:24 -0500

    add get all

--- server/api/stickers.js
+++ server/api/stickers.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const queries = require('../db/queries');
+
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Read you loud and clear'
+  queries.getAll().then(stickers =>{
+    res.json(stickers);
   });
 });
 
